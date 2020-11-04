8264f11922a4876a811e13739da8ea912d0764e6
h2o <h2o@example.com>
2020-11-04 11:14:00 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Description :
+@Time    : 2020/11/4 10:56
+@Author  : h2o
+@File    : main.py.py
+"""
+
+import numpy
+import scipy
+
+if __name__ == "__main__":
+    print(numpy.__version__)
+    print(scipy.__version__)
